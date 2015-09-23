d6c7c5ef8a53d31004932b10d465277b53242c85
Erkki Heino <erkki.heino@aalto.fi>
2015-09-23 17:29:02 +0300

    Better preprocessing in places.py

--- places.py
+++ places.py
@@ -1,77 +1,93 @@
+import re
 from arpa import Arpa, process
 from rdflib import Graph, URIRef
 
 def preprocessor(text):
-    return ", ".join(x.strip() for x in text.split("-"))
+    text = text.replace('Yli-Tornio', 'Ylitornio')
+    # Remove unit numbers that would otherwise be interpreted as Ii.
+    text = re.sub('II(I)*', '', text)
+    # Remove parentheses.
+    text = re.sub('[()]', ' ', text)
+    # Add a space after commas if they're followed by a word
+    text = re.sub(r'(\w),(\w)', r'\1, \2', text)
+    # Baseforming doesn't work for "Salla" so baseform that manually.
+    text = re.sub(r'Salla(a?n?|s[st]a)?\b', 'Salla', text)
+    # Detach names connected by hyphens to match places better.
+    # Skip Yl[äi]-, Al[ia]-, and Iso-.
+    text = re.sub(r'(?<!\b(Yl|Al|Is))(\w)-([A-ZÄÅÖ])', r'\2 \3', text)
 
-ignore = [
-    'sillanpää',
-    'saari',
-    'p',
-    'm',
-    's',
-    'pohjoinen',
-    'tienhaara',
-    'suomalainen',
-    'venäläinen',
-    'ii',
-    'asema',
-    'ns',
-    'rajavartiosto',
-    'esikunta',
-    'kauppa',
-    'ryhmä',
-    'ilma',
-    'olla',
-    'ruotsi',
-    'pakkanen',
-    'rannikko',
-    'koulu',
-    'kirkonkylä',
-    'saksa',
-    'työväentalo',
-    'kirkko',
-    'alku',
-    'lentokenttä',
-    'luoto',
-    'risti',
-    'posti',
-    'lehti',
-    'susi',
-    'tykki',
-    'prikaati',
-    'niemi',
-    'ranta',
-    'eteläinen',
-    'lappi',
-    'järvi',
-    'kallio',
-    'salainen',
-    'kannas',
-    'taavetti',
-    'berliini',
-    'hannula'
-]
+    return text
 
-no_duplicates = [
-    'http://www.yso.fi/onto/suo/kunta',
-    'http://ldf.fi/warsa/places/place_types/Kirkonkyla_kaupunki',
-    'http://ldf.fi/warsa/places/place_types/Kyla',
-    'http://ldf.fi/pnr-schema#place_type_560',
-    'http://ldf.fi/warsa/places/place_types/Maastokohde'
-]
+if __name__ == '__main__':
 
-graph = Graph()
-graph.parse('input.ttl', format='turtle')
+    ignore = [
+            'sillanpää',
+            'saari',
+            'p',
+            'm',
+            's',
+            'pohjoinen',
+            'tienhaara',
+            'suomalainen',
+            'venäläinen',
+            'asema',
+            'ns',
+            'rajavartiosto',
+            'esikunta',
+            'kauppa',
+            'ryhmä',
+            'ilma',
+            'olla',
+            'ruotsi',
+            'pakkanen',
+            'rannikko',
+            'koulu',
+            'kirkonkylä',
+            'saksa',
+            'työväentalo',
+            'kirkko',
+            'alku',
+            'lentokenttä',
+            'luoto',
+            'risti',
+            'posti',
+            'lehti',
+            'susi',
+            'tykki',
+            'prikaati',
+            'niemi',
+            'ranta',
+            'eteläinen',
+            'lappi',
+            'järvi',
+            'kallio',
+            'salainen',
+            'kannas',
+            'taavetti',
+            'berliini',
+            'hannula',
+            'itä'
+            ]
 
-arpa = Arpa('http://demo.seco.tkk.fi/arpa/warsa-event-place',
-        remove_duplicates=no_duplicates, ignore=ignore)
+    no_duplicates = [
+            'http://www.yso.fi/onto/suo/kunta',
+            'http://ldf.fi/warsa/places/place_types/Kirkonkyla_kaupunki',
+            'http://ldf.fi/warsa/places/place_types/Kyla',
+            'http://ldf.fi/pnr-schema#place_type_560',
+            'http://ldf.fi/warsa/places/place_types/Maastokohde'
+            ]
 
-# Query the ARPA service and add the matches
-process(graph, URIRef('http://www.cidoc-crm.org/cidoc-crm/P7_took_place_at'),
-        arpa, preprocessor=preprocessor, progress=True)
+    graph = Graph()
+    graph.parse('input.ttl', format='turtle')
 
-# Serialize the graph to disk
-graph.serialize(destination='output.ttl', format='turtle')
+    arpa = Arpa('http://demo.seco.tkk.fi/arpa/warsa-event-place',
+            remove_duplicates=no_duplicates, ignore=ignore)
+
+    # Query the ARPA service and add the matches
+    process(graph, URIRef('http://www.cidoc-crm.org/cidoc-crm/P7_took_place_at'),
+            arpa, preprocessor=preprocessor, progress=True)
+
+    # Serialize the graph to disk
+    graph.serialize(destination='output.ttl', format='turtle')
 
 
