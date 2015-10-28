eeba75cc608ac3349def8fd688fd0def1fb8597c
Erkki Heino <erkki.heino@aalto.fi>
2015-10-28 11:12:13 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="arpa_linker",
-    version="0.2.0",
+    version="0.2.1",
     author="Erkk Heino",
     description="Tool for linking rdf datasets to other datasets using ARPA",
     license="MIT",
